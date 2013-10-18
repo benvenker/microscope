ee2ff71529109a2d6ca586282986a80979138140
Ben Venker <bvemails@gmail.com>
2013-10-17 21:35:15 -0500

    Added edit posts form.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,4 +1,7 @@
 Template.postItem.helpers({
+	ownPost: function() {
+		return this.UserId == Meteor.userId();
+	},
 	domain: function() {
 		var a = document.createElement('a');
 		a.href = this.url;
