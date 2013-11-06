bd099aff353804cf62e9f4bb5e490c7d0b09279b
Ben Venker <bvemails@gmail.com>
2013-11-05 21:52:55 -0600

    Use the paginated subscription to step through posts.
    Also fixed pagination in general.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,19 @@
 Template.postsList.helpers({
-  posts: function() {
-    return Posts.find({}, {sort: {submitted: -1}});
-  }
+  	posts: function() {
+    return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit()})
+	},
+	postsReady: function() {
+	return postsHandle.ready();
+
+	},
+	allPostsLoaded: function() {
+	return postsHandle.ready() &&
+		Posts.find().count() < postsHandle.loaded();
+	}
+});
+
+Template.postsList.events({
+	'click .load-more': function(e) {
+		postsHandle.loadNextPage();
+	}
 });
