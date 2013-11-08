ca1b739604676bdfb003ba9500969ded65c17466
Ben Venker <bvemails@gmail.com>
2013-11-07 22:14:42 -0600

    Changed main.js to enable secure mode.
    Still having strange problems  - seemingly with spacing - that prevents
    main.js from working when I type it.

--- client/main.js
+++ client/main.js
@@ -11,20 +11,20 @@ Meteor.subscribe('notifications');
 
 Meteor.subscribe('currentUser');
 
-Deps.autorun(function() {
-	if (Meteor.user() && !Meteor.loggingIn()) {
-		Meteor.call('getHash', Metor.user().emails[0].address, function(error, result) {
-			Session.set('userEmailHash', result);
-		});
-		if(Session.get('userEmailHash')) {
-			var intercomSettings = {
-				email: Meteor.user().emails[0].address,
-				created_at: Math.round(Meteor.user().createdAt/1000),
-				user_name: Meteor.user().username,
-				user_hash: Session.get('userEmailHash'),
-				app_id: "2c3fa0169acc4f0e48ef4e5224a53036dd77c06a"
-			};
-			Intercom('boot', intercomSettings);
-		}		
-   }
+Deps.autorun(function(){
+  if (Meteor.user() && !Meteor.loggingIn()) {
+    Meteor.call('getHash', Meteor.user().emails[0].address, function(error, result){
+      Session.set('userEmailHash', result);
+    });
+    if(Session.get('userEmailHash')){
+      var intercomSettings = {
+        email: Meteor.user().emails[0].address,
+        created_at: Math.round(Meteor.user().createdAt/1000),
+        user_name: Meteor.user().username,
+        user_hash: Session.get('userEmailHash'),
+		app_id: "2c3fa0169acc4f0e48ef4e5224a53036dd77c06a"
+	  };
+      Intercom('boot', intercomSettings);
+    }
+  }
 });
