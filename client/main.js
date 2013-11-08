2bfaa4ab4c45bad3ce1a965b60fe066580e1e1d3
Ben Venker <bvemails@gmail.com>
2013-11-07 21:26:59 -0600

    Updated files to get Intercom working.
    Had some bad code in main.js.

--- client/main.js
+++ client/main.js
@@ -1,16 +1,18 @@
 newPostsHandle = Meteor.subscribeWithPagination('newPosts', 10);
 bestPostsHandle = Meteor.subscribeWithPagination('bestPosts', 10);
+
 Deps.autorun(function() {
   Meteor.subscribe('singlePost', Session.get('currentPostId'));
+  
   Meteor.subscribe('comments', Session.get('currentPostId'));
 })
 
 Meteor.subscribe('notifications');
 
-window.intercomSettings = {
-  // TODO: The current logged in user's email address.
-  email: "john.doe@example.com",
-  // TODO: The current logged in user's sign-up date as a Unix timestamp.
-  created_at: 1234567890,
-  app_id: "9b593f5995da6d2bd8f418754c0411d55201258a"
-};
+ window.intercomSettings = {
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+    app_id: "2c3fa0169acc4f0e48ef4e5224a53036dd77c06a"
+  };
--- packages/intercom/package.js
+++ packages/intercom/package.js
@@ -1,5 +1,9 @@
-Package.describe("Intercom package");
+Package.describe({
+  summary: "Intercom package"
+});
 
 Package.on_use(function (api) {
   api.add_files('intercom.js', 'client');
+  if(api.export)
+    api.export('Intercom');
 });
